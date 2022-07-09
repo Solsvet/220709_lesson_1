d0ececc21a2075de6ca8db79935f73be38025006
Solsvet <mamusik47@gmail.com>
2022-07-09 13:55:26 +0300

    Added Example003_Sum

--- Examples002_HelloUser/Program.cs
+++ Examples002_HelloUser/Program.cs
@@ -1,9 +1,9 @@
-﻿//Console.WriteLine("Введите ваше имя ");
-//string username = Console.ReadLine();
+﻿//Console.WriteLine("Введите ваше имя "); Вывод в одну строку и переход на новую строку
+//string username = Console.ReadLine(); 
 //Console.WriteLine("Привет, "); 
 //Console.WriteLine(username);
 
-Console.Write("Введите ваше имя ");
-string username = Console.ReadLine();
+Console.Write("Введите ваше имя "); // Вывод в одну строку без перехода на новую строку
+string username = Console.ReadLine(); // Считывает строку из терминала
 Console.Write("Привет, "); 
 Console.Write(username);
